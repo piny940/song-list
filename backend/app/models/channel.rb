c64dad0471b170db07a02e0e0b51ff7b957c5e6a
piny940 <an.s.shoma@gmail.com>
2023-06-01 19:47:06 +0900

    Channelのインスタンスメソッドをいくつか追加

--- backend/app/models/channel.rb
+++ backend/app/models/channel.rb
@@ -1,5 +1,4 @@
 class Channel < ApplicationRecord
-
   def self.fetch_and_create!(channel_id)
     response = Youtube.get_channel(channel_id)
 
@@ -10,7 +9,38 @@ class Channel < ApplicationRecord
     Channel.create!(
       channel_id:,
       name: items[0].snippet.title,
-      response_json: response.to_json
+      response_json: items[0].to_json
     )
+  end
+
+  # {
+  #   default: {
+  #     width: number,
+  #     height: number,
+  #     url: string,
+  #   },
+  #   medium: {
+  #     width: number,
+  #     height: number,
+  #     url: string,
+  #   },
+  #   high: {
+  #     width: number,
+  #     height: number,
+  #     url: string,
+  #   },
+  # }
+  def thumnails
+    hash = JSON.parse(response_json)
+    hash["snippet"]["thumnails"]
+  end
+
+  def description
+    hash = JSON.parse(response_json)
+    hash["snippet"]["description"]
+  end
+
+  def custom_id
+    hash["snippet"]["customUrl"]
   end
 end
