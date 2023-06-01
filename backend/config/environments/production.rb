4322befcd5dc4249fdf4268936fac824991f871c
piny940 <an.s.shoma@gmail.com>
2023-06-01 10:56:56 +0900

    app名を修正

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "rails_template_production"
+  # config.active_job.queue_name_prefix = "backend_song_list_production"
 
   config.action_mailer.perform_caching = false
 
