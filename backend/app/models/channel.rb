28e0f2777d1561b557aebf111bf1aaf1a2d6eb4a
piny940 <an.s.shoma@gmail.com>
2023-06-01 19:39:01 +0900

    Channel.fetch_and_createを作成

--- backend/app/models/channel.rb
+++ backend/app/models/channel.rb
@@ -1,2 +1,16 @@
 class Channel < ApplicationRecord
+
+  def self.fetch_and_create!(channel_id)
+    response = Youtube.get_channel(channel_id)
+
+    items = response.items
+
+    return if items.blank?
+
+    Channel.create!(
+      channel_id:,
+      name: items[0].snippet.title,
+      response_json: response.to_json
+    )
+  end
 end
--- backend/app/models/concerns/youtube.rb
+++ backend/app/models/concerns/youtube.rb
@@ -2,21 +2,18 @@ require 'google/apis/youtube_v3'
 require 'open-uri'
 
 class Youtube
-  def initialize
-    @service = Google::Apis::YoutubeV3::YouTubeService.new
-    @service.key = ENV['GOOGLE_API_KEY']
+  extend ActiveSupport::Concern
+
+  def self.get_channel(channel_id)
+    service.list_channels('snippet', id: channel_id)
   end
 
-  def get_channel(channel_id)
-    @service.list_channels('snippet', id: channel_id)
+  def self.get_video(video_id)
+    service.list_videos('liveStreamingDetails,snippet', id: video_id)
   end
 
-  def get_video(video_id)
-    @service.list_videos('liveStreamingDetails,snippet', id: video_id)
-  end
-
-  def get_chat_id(video_id)
-    data = @service.list_videos('liveStreamingDetails', id: video_id)
+  def self.get_chat_id(video_id)
+    data = service.list_videos('liveStreamingDetails', id: video_id)
 
     details = data.items[0].live_streaming_details
     return details.active_live_chat_id if details.respond_to? :active_live_chat_id
@@ -24,7 +21,15 @@ class Youtube
     None
   end
 
-  def get_chat_messages_data(chat_id, page_token: nil)
-    @service.list_live_chat_messages(chat_id, 'id,snippet,authorDetails', page_token:)
+  def self.get_chat_messages_data(chat_id, page_token: nil)
+    service.list_live_chat_messages(chat_id, 'id,snippet,authorDetails', page_token:)
+  end
+
+  private
+
+  def self.service
+    service = Google::Apis::YoutubeV3::YouTubeService.new
+    service.key = ENV['GOOGLE_API_KEY']
+    service
   end
 end
