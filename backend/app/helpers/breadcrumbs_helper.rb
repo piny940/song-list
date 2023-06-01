6f1f1322fb266279f23b297e73371b0b94bee90b
piny940 <an.s.shoma@gmail.com>
2023-06-01 19:55:33 +0900

    パンくずメニューを作成

--- backend/app/helpers/breadcrumbs_helper.rb
+++ backend/app/helpers/breadcrumbs_helper.rb
@@ -10,7 +10,8 @@ module BreadcrumbsHelper
 
   def breadcrumbs_tree
     [
-      ['admin/homes#show']
+      ['admin/homes#show', 'admin/channels#index', 'admin/channels#new'],
+      ['admin/homes#show', 'admin/channels#index', 'admin/channels#show', 'admin/channels#edit'],
     ]
   end
 
@@ -18,6 +19,14 @@ module BreadcrumbsHelper
     case name
     when 'admin/homes#show'
       { name: '管理者ページ', url: admin_path }
+    when 'admin/channels#index'
+      { name: 'チャンネル一覧', url: admin_channels_path }
+    when 'admin/channels#show'
+      { name: @channel.name, url: admin_channel_path(@channel) }
+    when 'admin/channels#new'
+      { name: '新規チャンネル作成', url: new_admin_channel_path }
+    when 'admin/channels#edit'
+      { name: "編集", url: edit_admin_channel_path(@channel) }
     else
       raise "breadcrumbs_url に #{name} が定義されていません！"
     end
