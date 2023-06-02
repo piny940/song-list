4b77e4b94111b940bfd2e350cb4d66abeaf3fd44
piny940 <an.s.shoma@gmail.com>
2023-06-02 16:02:21 +0900

    videos#showのテストを作成

--- backend/spec/requests/videos_spec.rb
+++ backend/spec/requests/videos_spec.rb
@@ -22,4 +22,22 @@ describe Api::VideosController do
       expect(json['videos'][0]['description']).to eq video.description
     end
   end
+  describe 'GET /api/videos/:id' do
+    let(:endpoint) { "/api/channels/#{channel.id}/videos" }
+    it '正常に取得できる' do
+      get "#{endpoint}/#{video.id}"
+
+      expect(response.status).to eq 200
+
+      json = response.parsed_body
+
+      expect(json['video']['id']).to eq video.id
+      expect(json['video']['video_id']).to eq video.video_id
+      expect(json['video']['channel_id']).to eq channel.id
+      expect(json['video']['kind']).to eq video.kind
+      expect(json['video']['title']).to eq video.title
+      expect(json['video']['thumbnails']['medium']['width']).to eq 320
+      expect(json['video']['description']).to eq video.description
+    end
+  end
 end
