9a280f2ce1d5cd3473a0917f7fcc31148c786a59
Mikan <83708535+piny940@users.noreply.github.com>
2023-06-02 16:03:46 +0900

    Videos APIを作成 (#9)
    * VideosControllerを作成
    
    * videos_controller#indexを作成
    
    * videos#indexのテストを作成
    
    * videos#showのテストを作成
    
    * videos#showを作成

---
+++ backend/app/views/api/videos/_video.json.jbuilder
@@ -0,0 +1,3 @@
+json.extract! video, :id, :video_id, :title, :kind, :channel_id
+json.thumbnails video.thumbnails
+json.description video.description
---
+++ backend/spec/requests/videos_spec.rb
@@ -0,0 +1,43 @@
+describe Api::VideosController do
+  fixtures :channels, :videos
+  let(:channel) { channels(:one) }
+  let(:video) { videos(:one) }
+
+  describe 'GET /api/videos' do
+    let(:endpoint) { "/api/channels/#{channel.id}/videos" }
+    it '正常に取得できる' do
+      get endpoint
+
+      expect(response.status).to eq 200
+
+      json = response.parsed_body
+
+      expect(json['videos'].count).to eq 1
+      expect(json['videos'][0]['id']).to eq video.id
+      expect(json['videos'][0]['video_id']).to eq video.video_id
+      expect(json['videos'][0]['channel_id']).to eq channel.id
+      expect(json['videos'][0]['kind']).to eq video.kind
+      expect(json['videos'][0]['title']).to eq video.title
+      expect(json['videos'][0]['thumbnails']['medium']['width']).to eq 320
+      expect(json['videos'][0]['description']).to eq video.description
+    end
+  end
+  describe 'GET /api/videos/:id' do
+    let(:endpoint) { "/api/channels/#{channel.id}/videos" }
+    it '正常に取得できる' do
+      get "#{endpoint}/#{video.id}"
+
+      expect(response.status).to eq 200
+
+      json = response.parsed_body
+
+      expect(json['video']['id']).to eq video.id
+      expect(json['video']['video_id']).to eq video.video_id
+      expect(json['video']['channel_id']).to eq channel.id
+      expect(json['video']['kind']).to eq video.kind
+      expect(json['video']['title']).to eq video.title
+      expect(json['video']['thumbnails']['medium']['width']).to eq 320
+      expect(json['video']['description']).to eq video.description
+    end
+  end
+end
