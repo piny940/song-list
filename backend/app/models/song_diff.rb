f8622383448754c5fef23226af19843162309fe8
piny940 <an.s.shoma@gmail.com>
2023-06-02 20:43:21 +0900

    SongItemとSongDiffのインスタンスメソッドを整備

--- backend/app/models/song_diff.rb
+++ backend/app/models/song_diff.rb
@@ -1,4 +1,14 @@
 class SongDiff < ApplicationRecord
   belongs_to :song_item
   belongs_to :made_by, class_name: 'User'
+
+  enum status: {
+    pending: 0,
+    approved: 10,
+    rejected: 20
+  }, _prefix: true
+
+  def deletion?
+    time.blank? && title.blank? && author.blank?
+  end
 end
--- backend/app/models/song_item.rb
+++ backend/app/models/song_item.rb
@@ -1,4 +1,25 @@
 class SongItem < ApplicationRecord
   belongs_to :video
   has_many :song_diffs, dependent: :destroy
+
+  def active?
+    return false unless song_diffs.present?
+    
+    !song_diffs.status_approved.last.deletion?
+  end
+
+  def title
+    return nil unless active?
+    song_diffs.status_approved.last.title
+  end
+
+  def author
+    return nil unless active?
+    song_diffs.status_approved.last.author
+  end
+
+  def time
+    return nil unless active?
+    song_diffs.status_approved.last.time
+  end
 end
