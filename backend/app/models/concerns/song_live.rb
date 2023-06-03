b6452ba90acafe6d3ad12a9803063e0a4ebf2bbc
piny940 <an.s.shoma@gmail.com>
2023-06-03 10:44:35 +0900

    parse_setlistを作成

--- backend/app/models/concerns/song_live.rb
+++ backend/app/models/concerns/song_live.rb
@@ -10,4 +10,31 @@ module SongLive
   def song_live?
     !!title.match('歌枠')
   end
+
+  def parse_setlist(text)
+    instruction = <<~EOS
+      以下は歌枠のsetlistです。これを時間と曲名と作曲者名のリストに変換してください。
+
+      フォーマットは
+      [
+          {
+            title: "",
+            time: "",
+              author:""
+          }
+      ]
+      というフォーマットで書いてください。
+    EOS
+    messages = [
+      {
+        role: 'system',
+        content: instruction
+      },
+      {
+        role: 'assistant',
+        content: text
+      }
+    ]
+    OpenAi.complete_chat(messages)
+  end
 end
