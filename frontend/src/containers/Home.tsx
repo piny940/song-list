14ff9e7ea9807f3bdaf8345fffffaaa5c401b1b6
piny940 <an.s.shoma@gmail.com>
2023-06-05 08:40:33 +0900

    ListItemButtonを削除

--- frontend/src/containers/Home.tsx
+++ frontend/src/containers/Home.tsx
@@ -1,14 +1,17 @@
-import { ListItemButton } from '@/components/Common/ListItemButton'
 import { TestID } from '@/resources/TestID'
+import Link from 'next/link'
 
 export const Home: React.FC = () => {
   return (
     <div className="" data-testid={TestID.HOME}>
       <h1>歌枠データベース</h1>
       <div className="list-group">
-        <ListItemButton href="/channels" testID={TestID.HOME_CHANNELS_BUTTON}>
+        <Link
+          href="/channel"
+          className="list-group-item list-group-item-action"
+        >
           チャンネルから検索
-        </ListItemButton>
+        </Link>
       </div>
     </div>
   )
