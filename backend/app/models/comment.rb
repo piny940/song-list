acd0a311ba55c90d34539e7fc791cdd6e68ad3aa
piny940 <an.s.shoma@gmail.com>
2023-06-04 20:20:02 +0900

    Comment.statusを追加

--- backend/app/models/comment.rb
+++ backend/app/models/comment.rb
@@ -1,3 +1,11 @@
 class Comment < ApplicationRecord
   belongs_to :video
+
+  # セトリのコメントでない場合はcompleted、
+  # セトリのコメントの場合は、OpenAIで調べたらcompleted、エラーが起こったらfetched
+  enum status: {
+    ready: 0,
+    fetched: 10,
+    completed: 20
+  }
 end
