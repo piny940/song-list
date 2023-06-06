d782b690e4fe547aaa6876b03bce4663ba1fc7b5
piny940 <an.s.shoma@gmail.com>
2023-06-06 11:36:05 +0900

    getChannelsを作成

--- frontend/src/resources/types.ts
+++ frontend/src/resources/types.ts
@@ -1,1 +1,28 @@
 export type Theme = 'dark' | 'light'
+
+export type Thumbnails = {
+  default: {
+    url: string
+    width: number
+    height: number
+  }
+  medium: {
+    url: string
+    width: number
+    height: number
+  }
+  high: {
+    url: string
+    width: number
+    height: number
+  }
+}
+
+export type Channel = {
+  id: number
+  channel_id: string
+  name: string
+  twitter_id: string | null
+  thumbnails: Thumbnails
+  description: string
+}
