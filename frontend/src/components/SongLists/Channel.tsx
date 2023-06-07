8df55fac08c6f46041ba11f0700ccb12e59bcdc2
piny940 <an.s.shoma@gmail.com>
2023-06-07 19:43:28 +0900

    Channelボタンでチャンネルページに飛べるようにする

--- frontend/src/components/SongLists/Channel.tsx
+++ frontend/src/components/SongLists/Channel.tsx
@@ -1,6 +1,7 @@
 import { TestID } from '@/resources/TestID'
 import { Channel as ChannelType } from '@/resources/types'
 import Image from 'next/image'
+import Link from 'next/link'
 import styled from 'styled-components'
 
 const ChannelDiv = styled.div`
@@ -13,22 +14,24 @@ export type ChannelProps = {
 
 export const Channel: React.FC<ChannelProps> = ({ channel }) => {
   return (
-    <ChannelDiv
-      className="d-flex flex-grow-1 border border-light rounded m-1 p-2 shadow-sm"
-      data-testid={TestID.CHANNEL}
-    >
-      <div className="d-flex align-items-center">
-        <Image
-          alt="channel icon"
-          src={channel.thumbnails.default.url}
-          width={80}
-          height={80}
-          className="rounded-circle"
-        />
-      </div>
-      <div className="d-flex flex-column ms-3">
-        <span className="fw-bold mt-2 ms-2">{channel.name}</span>
-      </div>
-    </ChannelDiv>
+    <Link href={`/channels/${channel.id}`}>
+      <ChannelDiv
+        className="d-flex flex-grow-1 border border-light rounded m-1 p-2 shadow-sm"
+        data-testid={TestID.CHANNEL}
+      >
+        <div className="d-flex align-items-center">
+          <Image
+            alt="channel icon"
+            src={channel.thumbnails.default.url}
+            width={80}
+            height={80}
+            className="rounded-circle"
+          />
+        </div>
+        <div className="d-flex flex-column ms-3">
+          <span className="fw-bold mt-2 ms-2">{channel.name}</span>
+        </div>
+      </ChannelDiv>
+    </Link>
   )
 }
