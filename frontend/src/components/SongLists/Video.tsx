f1328413128eaaa3a6d12ad6d4344100a72da57b
piny940 <an.s.shoma@gmail.com>
2023-06-07 21:25:40 +0900

    Videoの見た目を修正

--- frontend/src/components/SongLists/Video.tsx
+++ frontend/src/components/SongLists/Video.tsx
@@ -1,6 +1,9 @@
 import { TestID } from '@/resources/TestID'
 import { Video as VideoType } from '@/resources/types'
 import Image from 'next/image'
+import { styled } from 'styled-components'
+
+const VideoDiv = styled.div``
 
 export type VideoProps = {
   video: VideoType
@@ -14,11 +17,12 @@ export const Video: React.FC<VideoProps> = ({ video, type }) => {
       : { width: '300px', height: '100px' }
 
   return (
-    <div
+    <VideoDiv
       className="video border border-light shadow-sm m-1"
       data-testid={TestID.VIDEO}
       style={{ ...size }}
     >
+      <div className="p-2">{video.title}</div>
       <div className="">
         <Image
           src={video.thumbnails.high.url}
@@ -27,6 +31,6 @@ export const Video: React.FC<VideoProps> = ({ video, type }) => {
           alt="Video thumbnail"
         />
       </div>
-    </div>
+    </VideoDiv>
   )
 }
