61abea3da3c2799c49102033a39e3e1e7389487e
piny940 <an.s.shoma@gmail.com>
2023-06-09 00:42:47 +0900

    Videoのページングをできるようにする

--- frontend/src/components/SongLists/Videos.tsx
+++ frontend/src/components/SongLists/Videos.tsx
@@ -5,6 +5,8 @@ import Error from 'next/error'
 import useSWR from 'swr'
 import { Video } from './Video'
 import { Loading } from '../Common/Loading'
+import { useState } from 'react'
+import { Paging } from '../Common/Paging'
 
 export type VideosProps = {
   channel: ChannelType
@@ -12,33 +14,45 @@ export type VideosProps = {
 }
 
 export const Videos: React.FC<VideosProps> = ({ channel, type }) => {
-  const { data, error } = useSWR<{ videos: VideoType[] }>(
-    `/channels/${channel.id}/videos`,
+  const [page, setPage] = useState(1)
+  const { data, error } = useSWR<{ videos: VideoType[]; total_pages: number }>(
+    `/channels/${channel.id}/videos?` +
+      new URLSearchParams({
+        count: '10',
+        page: String(page),
+      }).toString(),
     getData
   )
   if (error) return <Error statusCode={404} />
 
   return data ? (
-    type === 'large' ? (
-      <div
-        className="videos row row-cols-lg-2 row-cols-xl-3"
-        data-testid={TestID.VIDEOS}
-      >
-        {data.videos.map((video) => (
-          <div className="" key={video.id}>
-            <Video type={type} video={video} />
-          </div>
-        ))}
-      </div>
-    ) : (
-      <div className="videos" data-testid={TestID.VIDEOS}>
-        {data.videos.map((video) => (
-          <div className="" key={video.id}>
-            <Video type={type} video={video} />
-          </div>
-        ))}
-      </div>
-    )
+    <div className="">
+      {type === 'large' ? (
+        <div
+          className="videos row row-cols-lg-2 row-cols-xl-3 mb-4"
+          data-testid={TestID.VIDEOS}
+        >
+          {data.videos.map((video) => (
+            <div className="" key={video.id}>
+              <Video type={type} video={video} />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="videos mb-4" data-testid={TestID.VIDEOS}>
+          {data.videos.map((video) => (
+            <div className="" key={video.id}>
+              <Video type={type} video={video} />
+            </div>
+          ))}
+        </div>
+      )}
+      <Paging
+        setPageNumber={setPage}
+        totalPages={data.total_pages}
+        currentPage={page}
+      />
+    </div>
   ) : (
     <Loading />
   )
