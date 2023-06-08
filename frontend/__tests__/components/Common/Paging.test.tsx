12a394c0a332d0646b0908c26a5ee5ca3085a31c
piny940 <an.s.shoma@gmail.com>
2023-06-08 23:16:20 +0900

    Pagingのテストを作成

---
+++ frontend/__tests__/components/Common/Paging.test.tsx
@@ -0,0 +1,15 @@
+import { render, waitFor } from '@testing-library/react'
+import { expect } from '@jest/globals'
+import { Paging, PagingProps } from '@/components/Common/Paging'
+import { Mock } from 'ts-mockery'
+
+describe('<Paging />', () => {
+  it('正常に描画される', async () => {
+    const props = Mock.from<PagingProps>({})
+    const component = render(<Paging {...props} />)
+
+    await waitFor(() => {
+      expect(component).toBeTruthy()
+    })
+  })
+})
