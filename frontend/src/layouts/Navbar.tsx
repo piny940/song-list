a6e70fcbfc5d5956e229372a6aca2caa922a259c
Mikan <83708535+piny940@users.noreply.github.com>
2023-06-09 00:26:59 +0900

    ロゴを作成 (#56)

--- frontend/src/layouts/Navbar.tsx
+++ frontend/src/layouts/Navbar.tsx
@@ -1,5 +1,6 @@
 import { IconButton } from '@/components/Navbar/IconButton'
 import { TestID } from '@/resources/TestID'
+import Image from 'next/image'
 import Link from 'next/link'
 
 export const Navbar: React.FC = () => {
@@ -9,8 +10,18 @@ export const Navbar: React.FC = () => {
       className="navbar navbar-expand navbar-light bg-light"
     >
       <div className="container-fluid px-5">
-        <Link href="/" className="navbar-brand title fw-bold">
-          Song Lists
+        <Link
+          href="/"
+          className="navbar-brand title fw-bold d-flex align-items-center"
+        >
+          <Image
+            src="/images/icon.png"
+            className="rounded-3"
+            alt="icon"
+            width={35}
+            height={35}
+          />
+          <span className="ms-2">Song Lists</span>
         </Link>
         <div className="navbar-nav w-100 mx-3">
           <div className="nav-item">
