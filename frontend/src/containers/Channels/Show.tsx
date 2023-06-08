603a4eff2d63834a91d33e117db24f1ab54617ba
Mikan <83708535+piny940@users.noreply.github.com>
2023-06-08 20:08:45 +0900

    歌を検索できるようにする (#44)

--- frontend/src/containers/Channels/Show.tsx
+++ frontend/src/containers/Channels/Show.tsx
@@ -3,6 +3,7 @@ import { Videos } from '@/components/SongLists/Videos'
 import { ChannelType } from '@/resources/types'
 import { getData } from '@/utils/api'
 import Error from 'next/error'
+import { useState } from 'react'
 import useSWR from 'swr'
 
 export type ChannelsShowProps = {
@@ -10,8 +11,10 @@ export type ChannelsShowProps = {
 }
 
 export const ChannelsShow: React.FC<ChannelsShowProps> = ({ id }) => {
+  const [query, setQuery] = useState('')
+
   const { data, error } = useSWR<{ channel: ChannelType }>(
-    `/channels/${id}`,
+    `/channels/${id}?`,
     getData
   )
 
@@ -21,8 +24,21 @@ export const ChannelsShow: React.FC<ChannelsShowProps> = ({ id }) => {
     <div className="channel">
       <h1>{data.channel.name}</h1>
       <div className="d-flex">
-        <div className="w-50">
-          <SongItems />
+        <div className="w-50 px-3">
+          <div className="row px-4 my-2">
+            <div className="fw-bold col-2 col-form-label">検索</div>
+            <div className="col-10">
+              <input
+                type="text"
+                className="form-control"
+                value={query}
+                onChange={(e) => {
+                  setQuery(e.target.value)
+                }}
+              />
+            </div>
+          </div>
+          <SongItems query={query} />
         </div>
         <div className="w-50">
           <div className="text-sm fw-bold w-100 text-center">配信から検索</div>
