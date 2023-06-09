a98a186aa52cf74e1f666b6dfa98be3b1a64931d
Mikan <83708535+piny940@users.noreply.github.com>
2023-06-09 20:08:30 +0900

    歌一覧を配信でグループ分けする (#67)
    * SongItemsを動画ごとにグルーピングできるようにする
    
    * 見た目を修正

--- frontend/src/components/SongLists/SongItems.tsx
+++ frontend/src/components/SongLists/SongItems.tsx
@@ -1,5 +1,5 @@
 import { TestID } from '@/resources/TestID'
-import { SongItemType } from '@/resources/types'
+import { SongItemType, VideoType } from '@/resources/types'
 import { getData } from '@/utils/api'
 import Error from 'next/error'
 import useSWR from 'swr'
@@ -7,6 +7,15 @@ import { SongItem } from './SongItem'
 import { Loading } from '../Common/Loading'
 import { useEffect, useState } from 'react'
 import { Paging } from '../Common/Paging'
+import { styled } from 'styled-components'
+
+const VideoTitleDiv = styled.div`
+  height: 20px;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 1;
+`
 
 export type SongItemsProps = {
   videoId?: string
@@ -35,15 +44,34 @@ export const SongItems: React.FC<SongItemsProps> = ({ videoId, query }) => {
 
   if (error) return <Error statusCode={404} />
 
+  const videos: { [videoId in string]: VideoType } = {}
+  const songItems: { [videoId in string]: SongItemType[] } = {}
+  for (const song of data?.song_items || []) {
+    const videoId = song.video.video_id
+    if (!Object.keys(videos).includes(videoId)) {
+      videos[videoId] = song.video
+      songItems[videoId] = [song]
+    } else {
+      songItems[videoId].push(song)
+    }
+  }
+
   return data ? (
     <div className="">
-      <div className="mb-4" data-testid={TestID.SONG_ITEMS}>
-        {data.song_items.map((songItem) => (
-          <div key={songItem.id}>
-            <SongItem songItem={songItem} />
+      {Object.values(videos).map((video) => (
+        <div className="" key={video.video_id}>
+          <VideoTitleDiv className="w-75">
+            <span className="small text-muted">{video.title}</span>
+          </VideoTitleDiv>
+          <div className="mb-4 ps-3" data-testid={TestID.SONG_ITEMS}>
+            {songItems[video.video_id].map((songItem) => (
+              <div key={songItem.id}>
+                <SongItem songItem={songItem} />
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
       <Paging
         currentPage={page}
         totalPages={data.total_pages}
