92f9eced0da5e6559c6c8355b29bb7984c2ca5e8
piny940 <an.s.shoma@gmail.com>
2023-06-10 23:06:46 +0900

    詳細検索のフォームを出し入れできるようにする

--- frontend/src/components/SongLists/SongItemsSearch.tsx
+++ frontend/src/components/SongLists/SongItemsSearch.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react'
+import styles from '../../styles/song-lists.module.scss'
+
 export type SongItemsSearchProps = {
   query: string
   setQuery: (query: string) => void
@@ -19,6 +22,10 @@ export const SongItemsSearch: React.FC<SongItemsSearchProps> = ({
   videoTitle,
   setVideoTitle,
 }) => {
+  const [detailOpened, setDetailOpened] = useState(false)
+
+  const toggleOpened = () => setDetailOpened(!detailOpened)
+
   return (
     <div className="song-search mb-3">
       <div className="row px-4 mt-2 mb-2">
@@ -36,47 +43,55 @@ export const SongItemsSearch: React.FC<SongItemsSearchProps> = ({
         </div>
       </div>
       <div className="detail-search my-2 px-4">
-        <a role="button" className="">
+        <a role="button" className="" onClick={toggleOpened}>
           <span>&#9654;</span>
           <span className="ms-1">詳細検索</span>
         </a>
-        <div className="m-2" id="song-detail-search">
-          <label className="row my-1">
-            <div className="col-3 fw-bold col-form-label">開始日</div>
-            <div className="col-9">
-              <input
-                type="date"
-                name="since"
-                className="form-control"
-                value={since}
-                onChange={(e) => setSince(e.target.value)}
-              />
+        <div
+          className={`${styles.collapsable} ${
+            detailOpened ? styles.active + ' border border-light rounded' : ''
+          }`}
+        >
+          {detailOpened && (
+            <div className="mx-2" id="song-detail-search">
+              <label className="row my-1">
+                <div className="col-3 fw-bold col-form-label">開始日</div>
+                <div className="col-9">
+                  <input
+                    type="date"
+                    name="since"
+                    className="form-control"
+                    value={since}
+                    onChange={(e) => setSince(e.target.value)}
+                  />
+                </div>
+              </label>
+              <label className="row my-1">
+                <div className="col-3 fw-bold col-form-label">終了日</div>
+                <div className="col-9">
+                  <input
+                    type="date"
+                    name="until"
+                    className="form-control"
+                    value={until}
+                    onChange={(e) => setUntil(e.target.value)}
+                  />
+                </div>
+              </label>
+              <label className="row my-1">
+                <div className="col-3 fw-bold col-form-label">枠名</div>
+                <div className="col-9">
+                  <input
+                    type="text"
+                    name="videoTitle"
+                    className="form-control"
+                    value={videoTitle}
+                    onChange={(e) => setVideoTitle(e.target.value)}
+                  />
+                </div>
+              </label>
             </div>
-          </label>
-          <label className="row my-1">
-            <div className="col-3 fw-bold col-form-label">終了日</div>
-            <div className="col-9">
-              <input
-                type="date"
-                name="until"
-                className="form-control"
-                value={until}
-                onChange={(e) => setUntil(e.target.value)}
-              />
-            </div>
-          </label>
-          <label className="row my-1">
-            <div className="col-3 fw-bold col-form-label">枠名</div>
-            <div className="col-9">
-              <input
-                type="text"
-                name="videoTitle"
-                className="form-control"
-                value={videoTitle}
-                onChange={(e) => setVideoTitle(e.target.value)}
-              />
-            </div>
-          </label>
+          )}
         </div>
       </div>
     </div>
