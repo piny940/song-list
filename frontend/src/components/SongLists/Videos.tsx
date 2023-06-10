5662f8584a601e68378c5f8714a80d9a5f444684
piny940 <an.s.shoma@gmail.com>
2023-06-11 04:23:13 +0900

    見た目を修正

--- frontend/src/components/SongLists/Videos.tsx
+++ frontend/src/components/SongLists/Videos.tsx
@@ -41,20 +41,22 @@ export const Videos: React.FC<VideosProps> = ({
   if (error) return <Error statusCode={404} />
 
   return data ? (
-    <div className="videos mb-4" data-testid={TestID.VIDEOS}>
+    <div className="videos" data-testid={TestID.VIDEOS}>
       {data.videos.length > 0 ? (
         <>
-          {data.videos.map((video) => (
-            <Video
-              songListOpen={openedVideo?.id === video.id}
-              video={video}
-              key={video.id}
-              toggleSongListOpened={() => {
-                if (openedVideo === video) setOpenedVideo(null)
-                else setOpenedVideo(video)
-              }}
-            />
-          ))}
+          <div className="mb-4">
+            {data.videos.map((video) => (
+              <Video
+                songListOpen={openedVideo?.id === video.id}
+                video={video}
+                key={video.id}
+                toggleSongListOpened={() => {
+                  if (openedVideo === video) setOpenedVideo(null)
+                  else setOpenedVideo(video)
+                }}
+              />
+            ))}
+          </div>
           <Paging
             setPageNumber={setPage}
             totalPages={data.total_pages}
