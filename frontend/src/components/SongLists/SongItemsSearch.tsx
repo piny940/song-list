4b2868b358e696437aea70fb0c700467885e2561
piny940 <an.s.shoma@gmail.com>
2023-06-10 21:53:17 +0900

    SongItemsSearchを作成

---
+++ frontend/src/components/SongLists/SongItemsSearch.tsx
@@ -0,0 +1,66 @@
+export type SongItemsSearchProps = {
+  query: string
+  setQuery: (query: string) => void
+  since: string
+  setSince: (since: string) => void
+  until: string
+  setUntil: (until: string) => void
+  videoTitle: string
+  setVideoTitle: (videoTitle: string) => void
+}
+
+export const SongItemsSearch: React.FC<SongItemsSearchProps> = ({
+  query,
+  setQuery,
+  since,
+  setSince,
+  until,
+  setUntil,
+  videoTitle,
+  setVideoTitle,
+}) => {
+  return (
+    <div className="song-search">
+      <div className="row px-4 mt-2 mb-4">
+        <div className="fw-bold col-2 col-form-label">検索</div>
+        <div className="col-10">
+          <input
+            type="text"
+            className="form-control"
+            value={query}
+            placeholder="曲名/歌手名を入力"
+            onChange={(e) => {
+              setQuery(e.target.value)
+            }}
+          />
+        </div>
+      </div>
+      <div className="detail-search my-2 px-4">
+        <a role="button" className="">
+          <span>&#9654;</span>
+          <span className="ms-1">詳細検索</span>
+        </a>
+        <div className="m-2" id="song-detail-search">
+          <label className="row my-1">
+            <div className="col-3 fw-bold col-form-label">開始日</div>
+            <div className="col-9">
+              <input type="date" name="since" className="form-control" />
+            </div>
+          </label>
+          <label className="row my-1">
+            <div className="col-3 fw-bold col-form-label">終了日</div>
+            <div className="col-9">
+              <input type="date" name="until" className="form-control" />
+            </div>
+          </label>
+          <label className="row my-1">
+            <div className="col-3 fw-bold col-form-label">枠名</div>
+            <div className="col-9">
+              <input type="text" name="videoTitle" className="form-control" />
+            </div>
+          </label>
+        </div>
+      </div>
+    </div>
+  )
+}
--- frontend/src/containers/Channels/Show.tsx
+++ frontend/src/containers/Channels/Show.tsx
@@ -1,5 +1,6 @@
 import { Loading } from '@/components/Common/Loading'
 import { SongItems } from '@/components/SongLists/SongItems'
+import { SongItemsSearch } from '@/components/SongLists/SongItemsSearch'
 import { Videos } from '@/components/SongLists/Videos'
 import { ChannelType } from '@/resources/types'
 import { getData } from '@/utils/api'
@@ -30,52 +31,16 @@ export const ChannelsShow: React.FC<ChannelsShowProps> = ({ id }) => {
       <div className="d-flex">
         <div className="w-50 px-2">
           <div className="text-sm fw-bold w-100 text-center">歌一覧</div>
-          <div className="song-search">
-            <div className="row px-4 mt-2 mb-4">
-              <div className="fw-bold col-2 col-form-label">検索</div>
-              <div className="col-10">
-                <input
-                  type="text"
-                  className="form-control"
-                  value={songQuery}
-                  placeholder="曲名/歌手名を入力"
-                  onChange={(e) => {
-                    setSongQuery(e.target.value)
-                  }}
-                />
-              </div>
-            </div>
-            <div className="detail-search my-2 px-4">
-              <a role="button" className="">
-                <span>&#9654;</span>
-                <span className="ms-1">詳細検索</span>
-              </a>
-              <div className="m-2" id="song-detail-search">
-                <label className="row my-1">
-                  <div className="col-3 fw-bold col-form-label">開始日</div>
-                  <div className="col-9">
-                    <input type="date" name="since" className="form-control" />
-                  </div>
-                </label>
-                <label className="row my-1">
-                  <div className="col-3 fw-bold col-form-label">終了日</div>
-                  <div className="col-9">
-                    <input type="date" name="until" className="form-control" />
-                  </div>
-                </label>
-                <label className="row my-1">
-                  <div className="col-3 fw-bold col-form-label">枠名</div>
-                  <div className="col-9">
-                    <input
-                      type="text"
-                      name="videoTitle"
-                      className="form-control"
-                    />
-                  </div>
-                </label>
-              </div>
-            </div>
-          </div>
+          <SongItemsSearch
+            query={songQuery}
+            setQuery={setSongQuery}
+            since={songSince}
+            setSince={setSongSince}
+            until={songUntil}
+            setUntil={setSongUntil}
+            videoTitle={songVideoTitle}
+            setVideoTitle={setSongVideoTitle}
+          />
           <SongItems channelId={data.channel.id} query={songQuery} />
         </div>
         <div className="w-50 px-2">
