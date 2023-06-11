ca4e1c7d3eb4c611f8b1a274e9de5639c7aeb561
piny940 <an.s.shoma@gmail.com>
2023-06-11 15:26:45 +0900

    FormGroupを作成

---
+++ frontend/src/components/Common/FormGroup.tsx
@@ -0,0 +1,35 @@
+import { FieldValues, UseFormRegister } from 'react-hook-form'
+
+export type FormGroupProps = {
+  label: string
+  register: UseFormRegister<FieldValues>
+  type: string
+  name: string
+  required?: boolean
+}
+
+export const FormGroup: React.FC<FormGroupProps> = ({
+  label,
+  register,
+  type,
+  name,
+  required,
+}) => {
+  return (
+    <div className="row my-3">
+      <div className="col-md-3 fw-bold col-form-label">
+        {label}
+        {required && <span className="text-danger">*</span>}
+      </div>
+      <div className="col-md-9">
+        <input
+          type={type}
+          {...register(name, {
+            required: required && 'このフィールドは必須です。',
+          })}
+          className="form-control"
+        />
+      </div>
+    </div>
+  )
+}
--- frontend/src/containers/User/New.tsx
+++ frontend/src/containers/User/New.tsx
@@ -1,3 +1,4 @@
+import { FormGroup } from '@/components/Common/FormGroup'
 import { fetchApi } from '@/utils/api'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
@@ -33,60 +34,34 @@ export const UserNew: React.FC = () => {
       {alert && <div className="alert alert-danger">{alert}</div>}
       <h1>アカウント作成</h1>
       <form className="container" onSubmit={handleSubmit(submit)}>
-        <div className="row my-3">
-          <div className="col-md-3 fw-bold col-form-label">
-            メールアドレス
-            <span className="text-danger">*</span>
-          </div>
-          <div className="col-md-9">
-            <input
-              type="email"
-              {...register('email', { required: 'このフィールドは必須です。' })}
-              className="form-control"
-            />
-          </div>
-        </div>
-        <div className="row my-3">
-          <div className="col-md-3 fw-bold col-form-label">
-            名前
-            <span className="text-danger">*</span>
-          </div>
-          <div className="col-md-9">
-            <input
-              type="text"
-              {...register('name', { required: 'このフィールドは必須です。' })}
-              className="form-control"
-            />
-          </div>
-        </div>
-        <div className="row my-3">
-          <div className="col-md-3 fw-bold col-form-label">
-            パスワード<span className="text-danger">*</span>
-          </div>
-          <div className="col-md-9">
-            <input
-              type="password"
-              className="form-control"
-              {...register('password', {
-                required: 'このフィールドは必須です。',
-              })}
-            />
-          </div>
-        </div>
-        <div className="row my-3">
-          <div className="col-md-3 fw-bold col-form-label">
-            パスワード(確認用)<span className="text-danger">*</span>
-          </div>
-          <div className="col-md-9">
-            <input
-              type="password"
-              className="form-control"
-              {...register('password_confirmation', {
-                required: 'このフィールドは必須です。',
-              })}
-            />
-          </div>
-        </div>
+        <FormGroup
+          label="メールアドレス"
+          register={register}
+          type="email"
+          name="email"
+          required
+        />
+        <FormGroup
+          label="名前"
+          register={register}
+          type="text"
+          name="name"
+          required
+        />
+        <FormGroup
+          label="パスワード"
+          register={register}
+          type="password"
+          name="password_confirmation"
+          required
+        />
+        <FormGroup
+          label="パスワード(確認用)"
+          register={register}
+          type="password"
+          name="password_confirmation"
+          required
+        />
         <div className="">
           <Link href="/session/new" className="text-primary py-0">
             &gt;アカウントをすでにお持ちの場合はこちら
