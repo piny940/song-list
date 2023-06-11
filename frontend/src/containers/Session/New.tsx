0764bf78f3b3e314fe1827364cb145014c94ac75
piny940 <an.s.shoma@gmail.com>
2023-06-11 14:26:15 +0900

    ログインフォームを作成

--- frontend/src/containers/Session/New.tsx
+++ frontend/src/containers/Session/New.tsx
@@ -1,3 +1,38 @@
 export const SessionNew: React.FC = () => {
-  return <div className="">ログイン</div>
+  return (
+    <div className="">
+      <h1>ログイン</h1>
+      <form className="container">
+        <div className="row my-3">
+          <div className="col-md-3 fw-bold col-form-label">メールアドレス</div>
+          <div className="col-md-9">
+            <input
+              type="email"
+              name="email"
+              id=""
+              className="form-control"
+              required
+            />
+          </div>
+        </div>
+        <div className="row my-3">
+          <div className="col-md-3 fw-bold col-form-label">パスワード</div>
+          <div className="col-md-9">
+            <input
+              type="password"
+              name="password"
+              id=""
+              className="form-control"
+              required
+            />
+          </div>
+        </div>
+        <div className="row my-3">
+          <button type="submit" className="btn btn-primary">
+            ログイン
+          </button>
+        </div>
+      </form>
+    </div>
+  )
 }
