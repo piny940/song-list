4cda9410cf948c74a360c2592a1609ef387adadc
piny940 <an.s.shoma@gmail.com>
2023-06-11 20:13:30 +0900

    400ページを作成

---
+++ frontend/src/pages/400.tsx
@@ -0,0 +1,15 @@
+import { TestID } from '@/resources/TestID'
+import Link from 'next/link'
+
+const Custom500: React.FC = () => {
+  return (
+    <div className="container" data-testid={TestID.CUSTOM500}>
+      <h1>エラーが発生しました。(400)</h1>
+      <p>
+        <Link href="/">ホームに戻る</Link>
+      </p>
+    </div>
+  )
+}
+
+export default Custom500
