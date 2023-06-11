3458068fc86301703c1313e3fdb1c9f876fa88f5
piny940 <an.s.shoma@gmail.com>
2023-06-11 12:53:45 +0900

    usersにnameカラムを追加

--- backend/db/migrate/20230528110031_add_column_to_user.rb
+++ backend/db/migrate/20230528110031_add_column_to_user.rb
@@ -1,5 +1,6 @@
 class AddColumnToUser < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :kind, :integer, default: 0, null: false
+    add_column :users, :name, :string, null: false
   end
 end
