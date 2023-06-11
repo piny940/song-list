0244c5d4bb5b81dc13f6a18cc07e00170c2fd2c4
piny940 <an.s.shoma@gmail.com>
2023-06-11 13:46:05 +0900

    エラー処理が正しく行われることを確認

--- backend/spec/requests/usres_spec.rb
+++ backend/spec/requests/usres_spec.rb
@@ -39,5 +39,32 @@ describe Api::UsersController do
       expect(User.count).to eq (before_count)
       expect(json['message']).to eq '名前を入力してください'
     end
+
+    it('passwordは空欄ではいけない') do
+      before_count = User.count
+      post endpoint, params: { user: { email: 'alice@example.com', name: 'アリス', password: '', password_confirmation: '' }}
+      expect(response.status).to eq 400
+      json = response.parsed_body
+      expect(User.count).to eq (before_count)
+      expect(json['message']).to eq 'パスワードを入力してください'
+    end
+
+    it('passwordは6字以上で無いといけないいけない') do
+      before_count = User.count
+      post endpoint, params: { user: { email: 'alice@example.com', name: 'アリス', password: 'pass', password_confirmation: 'pass' }}
+      expect(response.status).to eq 400
+      json = response.parsed_body
+      expect(User.count).to eq (before_count)
+      expect(json['message']).to eq 'パスワードは6文字以上で入力してください'
+    end
+
+    it('passwordとpassword_confirmationは同じではいけない') do
+      before_count = User.count
+      post endpoint, params: { user: { email: 'alice@example.com', name: 'アリス', password: 'password', password_confirmation: 'pass' }}
+      expect(response.status).to eq 400
+      json = response.parsed_body
+      expect(User.count).to eq (before_count)
+      expect(json['message']).to eq 'パスワード（確認用）とパスワードの入力が一致しません'
+    end
   end
 end
