d2fcabf501b7d9bedbb92834e05cece81ac6d580
piny940 <an.s.shoma@gmail.com>
2023-06-11 13:26:38 +0900

    userを作成できるようにする

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -1,5 +1,17 @@
 class Api::UsersController < Api::Base
   def create
-    
+    @user = User.new(user_params)
+    if !@user.save!
+      # return render json: {
+      #   message: @user.errors.full_messages[0]
+      # }
+    end
+    render status: 201
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :name)
   end
 end
--- backend/spec/requests/usres_spec.rb
+++ backend/spec/requests/usres_spec.rb
@@ -4,12 +4,12 @@ describe Api::UsersController do
   describe('POST /api/user') do
     it('正常にユーザーを作成できる') do
       before_count = User.count
-      post endpoint, params: { user: { email: 'test1@example.com', name: 'Test1', password: 'password', password_confirmation: 'password' }}
+      post endpoint, params: { user: { email: 'alice@example.com', name: 'Alice', password: 'password', password_confirmation: 'password' }}
       expect(response.status).to eq 201
       json = response.parsed_body
-      expect(User.count).toe eq (before_count + 1)
-      expect(json['user']['email']).to eq 'test1@example.com'
-      expect(json['user']['name']).to eq 'Test1'
+      expect(User.count).to eq (before_count + 1)
+      expect(json['user']['email']).to eq 'alice@example.com'
+      expect(json['user']['name']).to eq 'Alice'
     end
   end
 end
