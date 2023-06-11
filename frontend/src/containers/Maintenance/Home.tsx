d8fa753e38e54521cd5eb3491453ba9d0b48c1d3
piny940 <an.s.shoma@gmail.com>
2023-06-11 20:23:33 +0900

    ログインしてないときはログインページに飛ぶようにする

--- frontend/src/containers/Maintenance/Home.tsx
+++ frontend/src/containers/Maintenance/Home.tsx
@@ -1,4 +1,14 @@
+import { getData } from '@/utils/api'
+import Error from 'next/error'
+import { useRouter } from 'next/router'
+import useSWR from 'swr'
+
 export const MaintenanceHome: React.FC = () => {
+  const { data, isLoading, error } = useSWR('/user', getData)
+  const router = useRouter()
+
+  if (error) return <Error statusCode={400} />
+  if (!isLoading && !data) void router.push('/session/new')
   return (
     <div className="">
       <h1 className="sub">メンテナンス</h1>
