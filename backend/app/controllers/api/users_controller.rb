0cab8b38690f52f2bd100d38020cfb19d85313a5
piny940 <an.s.shoma@gmail.com>
2023-06-17 22:22:33 +0900

    rubocop

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -1,12 +1,12 @@
 class Api::UsersController < Api::Base
   def create
     @user = User.new(user_params)
-    if !@user.save
+    unless @user.save
       return render json: {
         message: @user.errors.full_messages[0]
-      }, status: 400
+      }, status: :bad_request
     end
-    render status: 201
+    render status: :created
   end
 
   private
