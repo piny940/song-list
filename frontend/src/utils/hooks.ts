9fb325508127c6fda8d2f5428e7662dea3b5a3cf
piny940 <an.s.shoma@gmail.com>
2023-06-20 00:53:36 +0900

    useSongItemsを作成

--- frontend/src/utils/hooks.ts
+++ frontend/src/utils/hooks.ts
@@ -1,6 +1,9 @@
 import { useRouter } from 'next/router'
 import { queryToSearchParams } from './helpers'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import useSWR from 'swr'
+import { SongItemType } from '@/resources/types'
+import { getData } from './api'
 
 export const usePaginate = (key: string, defaultPage = 1) => {
   const router = useRouter()
@@ -35,4 +38,53 @@ export const useHold = (timer: number) => {
     )
   }
   return { isReady, updateTimer }
+}
+
+export const useSongItems = ({
+  query,
+  channelId,
+  videoId,
+  since,
+  until,
+  videoTitle,
+}: {
+  query: string
+  channelId: string
+  videoId: string
+  since: string
+  until: string
+  videoTitle: string
+}) => {
+  const DEFAULT_PAGE = 1
+  const { getPage, setPage } = usePaginate('song-items-page', DEFAULT_PAGE)
+  const { isReady, updateTimer } = useHold(500)
+  const isFirst = useRef(true)
+  const { data, error } = useSWR<{
+    song_items: SongItemType[]
+    total_pages: number
+  }>(
+    '/song_items?' +
+      queryToSearchParams({
+        query: query || '',
+        since: since || '',
+        until: until || '',
+        video_title: videoTitle || '',
+        channel_id: channelId != null ? String(channelId) : '',
+        video_id: videoId != null ? String(videoId) : '',
+        count: '15',
+        page: String(getPage()),
+      }).toString(),
+    getData
+  )
+
+  useEffect(() => {
+    if (isFirst) {
+      isFirst.current = false
+      return
+    }
+    setPage(DEFAULT_PAGE)
+    updateTimer()
+  }, [query, since, until, videoTitle])
+
+  return { data: isReady ? data : undefined, error }
 }
