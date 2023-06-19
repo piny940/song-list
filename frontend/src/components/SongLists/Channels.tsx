be0a7849337bb3b668366b10ccbcbf7f3aa08276
piny940 <an.s.shoma@gmail.com>
2023-06-20 01:15:10 +0900

    channelsのロジックを抽出

--- frontend/src/components/SongLists/Channels.tsx
+++ frontend/src/components/SongLists/Channels.tsx
@@ -1,23 +1,24 @@
 import { ChannelType } from '@/resources/types'
-import { getData } from '@/utils/api'
-import useSWR from 'swr'
 import { Channel } from './Channel'
 import { Loading } from '../Common/Loading'
 
 export type ChannelsProps = {
   testID?: string
   link: string
+  channels: ChannelType[] | undefined
 }
 
-export const Channels: React.FC<ChannelsProps> = ({ testID, link }) => {
-  const { data } = useSWR<{ channels: ChannelType[] }>('/channels', getData)
-
-  return data ? (
+export const Channels: React.FC<ChannelsProps> = ({
+  testID,
+  link,
+  channels,
+}) => {
+  return channels ? (
     <div
       className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xxl-4"
       data-testid={testID}
     >
-      {data.channels.map((channel) => (
+      {channels.map((channel) => (
         <div className="col" key={channel.id}>
           <Channel link={link} channel={channel} />
         </div>
