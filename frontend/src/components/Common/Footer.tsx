de6999f3f7d39242b10d00dc1449859b97feadd7
piny940 <an.s.shoma@gmail.com>
2023-06-20 23:08:22 +0900

    メンテナンスに参加するボタンをfooterに移動

--- frontend/src/components/Common/Footer.tsx
+++ frontend/src/components/Common/Footer.tsx
@@ -1,5 +1,8 @@
+import { UserType } from '@/resources/types'
+import { getData } from '@/utils/api'
 import Link from 'next/link'
 import { styled } from 'styled-components'
+import useSWR from 'swr'
 
 const FooterTag = styled.footer`
   margin-top: 20px;
@@ -8,10 +11,12 @@ const FooterTag = styled.footer`
 `
 
 export const Footer: React.FC = () => {
+  const { data } = useSWR<{ user: UserType }>('/user', getData)
+
   return (
     <FooterTag className="footer text-center bg-secondary text-white p-4">
       <Link href="/maintenance" className="text-white">
-        メンテナンスする
+        {!!data && !!data.user ? 'メンテナンスする' : 'メンテナンスに参加する'}
       </Link>
       <br />
       <small>&copy;2023 piny940</small>
