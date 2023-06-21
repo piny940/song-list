bff5ced81c67590d52410e90bd17da219a78dfb1
mikan <83708535+piny940@users.noreply.github.com>
2023-06-21 14:01:12 +0900

    Inquiryコンポーネントを作成

--- frontend/src/containers/Inquiry.tsx
+++ frontend/src/containers/Inquiry.tsx
@@ -1,3 +1,27 @@
+import Link from 'next/link'
+
 export const Inquiry: React.FC = () => {
-  return <div className="">ほげ</div>
+  return (
+    <div className="">
+      <h1>お問い合わせ</h1>
+      <div className="container">
+        <p>
+          お問い合わせは
+          <Link
+            className="text-primary text-decoration-underline"
+            href="https://twitter.com/IchigoMacaronnn"
+          >
+            いちごまかろん
+          </Link>
+          まで
+        </p>
+        <p className="text-danger">
+          このサイトはファンが非公式に運営しているものです。
+          <br />
+          このサイトに関して配信者様本人へ
+          問い合わせをされるのはお控えください。
+        </p>
+      </div>
+    </div>
+  )
 }
