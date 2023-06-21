f06cf18062b3f0beda8e94dd8b670031731ab283
piny940 <an.s.shoma@gmail.com>
2023-06-21 23:42:37 +0900

    Alert, Alertsのテストを作成

---
+++ frontend/__tests__/components/Common/Alert.test.tsx
@@ -0,0 +1,38 @@
+import { act, fireEvent, render, waitFor } from '@testing-library/react'
+import { TestID } from '@/resources/TestID'
+import { AlertState } from '@/resources/enums'
+import { expect } from '@jest/globals'
+import { Alert, AlertProps } from '@/components/Common/Alert'
+import { Mock } from 'ts-mockery'
+
+jest.useFakeTimers()
+
+describe('<Alert />', () => {
+  it('SuccessのAlertが正常に表示され、closeボタンで削除できる', async () => {
+    const removeAlert = jest.fn()
+
+    const props = Mock.from<AlertProps>({
+      alert: {
+        state: AlertState.SUCCESS,
+      },
+      margin: 'm-0',
+      removeAlert: removeAlert,
+    })
+
+    const { getByTestId } = render(<Alert {...props} />)
+
+    await waitFor(() => {
+      expect(getByTestId(TestID.ALERT).classList).toContain('alert')
+      expect(getByTestId(TestID.ALERT).classList).toContain('alert-success')
+      expect(getByTestId(TestID.ALERT).classList).toContain('m-0')
+    })
+
+    act(() => {
+      fireEvent.click(getByTestId(TestID.ALERT_CLOSE))
+    })
+
+    await waitFor(() => {
+      expect(removeAlert).toBeCalled()
+    })
+  })
+})
