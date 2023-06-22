545f8bc0a5346d278cfe7760780c22915e887463
piny940 <an.s.shoma@gmail.com>
2023-06-22 19:17:42 +0900

    SongDiffのテストを作成

---
+++ frontend/__tests__/components/SongLists/SongDiff.test.tsx
@@ -0,0 +1,19 @@
+import { render, waitFor } from '@testing-library/react'
+import { expect } from '@jest/globals'
+import { SongDiff, SongDiffProps } from '@/components/SongLists/SongDiff'
+import { Mock } from 'ts-mockery'
+import { SongDiffType } from '@/resources/types'
+
+describe('<SongDiff />', () => {
+  it('正常に描画される', async () => {
+    const props = Mock.from<SongDiffProps>({
+      songDiff: Mock.from<SongDiffType>({
+        id: 1001,
+      }),
+    })
+    const component = render(<SongDiff {...props} />)
+    await waitFor(() => {
+      expect(component).toBeTruthy()
+    })
+  })
+})
