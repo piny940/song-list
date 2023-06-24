d652c0bc8cf9f368f38b9ce23853d410d050bdc6
piny940 <an.s.shoma@gmail.com>
2023-06-24 14:55:40 +0900

    動画を選択してセトリ作成できるようにする

--- backend/app/controllers/admin/videos/song_items/bulks_controller.rb
+++ backend/app/controllers/admin/videos/song_items/bulks_controller.rb
@@ -2,10 +2,12 @@ class Admin::Videos::SongItems::BulksController < Admin::Videos::Base
   def new; end
 
   def create
-    if params[:comment_id]
+    if params[:comment_id].present?
       song_items = Comment.find(params[:comment_id]).force_search_and_create_song_items!
+    elsif params[:comment].present?
+      song_items = @video.song_items.create_from_comment_content!(params[:comment])
     else
-      song_items = @video.song_items.create_from_comment_content!(params[:comment]) if params[:comment].present?
+      song_items = @video.search_and_create_song_items!
     end
     @video.update!(status: 'completed') if song_items.present?
     redirect_to admin_video_song_items_path(@video)
