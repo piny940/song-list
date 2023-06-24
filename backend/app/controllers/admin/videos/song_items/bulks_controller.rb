6cc086d94e6ca0229c0927f05719f996fe2224e8
piny940 <an.s.shoma@gmail.com>
2023-06-24 14:35:53 +0900

    歌一覧作成に使ったコメントはcompletedになるようにする

--- backend/app/controllers/admin/videos/song_items/bulks_controller.rb
+++ backend/app/controllers/admin/videos/song_items/bulks_controller.rb
@@ -2,7 +2,12 @@ class Admin::Videos::SongItems::BulksController < Admin::Videos::Base
   def new; end
 
   def create
-    @video.song_items.create_from_comment_content!(params[:comment]) if params[:comment].present?
+    if params[:comment_id]
+      song_items = Comment.find(params[:comment_id]).force_search_and_create_song_items!
+    else
+      song_items = @video.song_items.create_from_comment_content!(params[:comment]) if params[:comment].present?
+    end
+    @video.update!(status: 'completed') if song_items.present?
     redirect_to admin_video_song_items_path(@video)
   end
 end
