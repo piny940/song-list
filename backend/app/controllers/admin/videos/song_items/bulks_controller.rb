b78930c867295486c317d7e856d57ec7e4851044
piny940 <an.s.shoma@gmail.com>
2023-06-25 21:45:16 +0900

    SongItem一括作成時にvideoが勝手にcompletedになる問題を修正

--- backend/app/controllers/admin/videos/song_items/bulks_controller.rb
+++ backend/app/controllers/admin/videos/song_items/bulks_controller.rb
@@ -9,7 +9,7 @@ class Admin::Videos::SongItems::BulksController < Admin::Videos::Base
                  else
                    @video.search_and_create_song_items!
                  end
-    @video.update!(status: 'completed') if song_items.present?
+    @video.update!(status: 'song_items_created') if song_items.present?
     redirect_to admin_video_song_items_path(@video)
   end
 end
