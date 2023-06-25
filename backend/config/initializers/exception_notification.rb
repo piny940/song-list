518d1c63b86b71e4025adc192b4ca33abc130dbc
piny940 <an.s.shoma@gmail.com>
2023-06-25 20:40:47 +0900

    slackにメッセージを送信できるようにする

---
+++ backend/config/initializers/exception_notification.rb
@@ -0,0 +1,12 @@
+require 'exception_notification/rails'
+
+if Rails.env.production?
+  ExceptionNotification.configure do |config|
+    config.add_notifier :slack, {
+      username: 'エラー通知太郎',
+      icon_emoji: ':japanese_ogre:',
+      webhook_url: ENV.fetch('SLACK_WEBHOOK_URL', nil),
+      channel: '#song-list-errors'
+    }
+  end
+end
