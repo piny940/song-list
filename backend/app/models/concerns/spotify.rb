fc0645b06abff01d62344230b57149f868f0d446
piny940 <an.s.shoma@gmail.com>
2023-06-25 11:15:17 +0900

    get_songs_dataを作成

--- backend/app/models/concerns/spotify.rb
+++ backend/app/models/concerns/spotify.rb
@@ -3,7 +3,7 @@ require 'net/http'
 module Spotify
   TOKEN_ENDPOINT = 'https://accounts.spotify.com/api/token'
   def self.get_token
-    uri = URI(TOKEN_ENDPOINT)
+    uri = URI.parse(TOKEN_ENDPOINT)
     data = {
       grant_type: 'client_credentials',
       client_id: ENV.fetch('SPOTIFY_CLIENT_ID'),
@@ -12,5 +12,22 @@ module Spotify
     response = Net::HTTP.post_form(uri, data)
     json = JSON.parse(response.body)
     json['access_token']
+  end
+
+  SEARCH_ENDPOINT = 'https://api.spotify.com/v1/search'
+  def self.get_songs_data(title, limit=1, token=nil)
+    token ||= get_token
+    uri = URI.parse(SEARCH_ENDPOINT)
+    uri.query = URI.encode_www_form({
+      q: "track:#{title}",
+      limit: limit.to_s,
+      type: 'track'
+    })
+    headers = {
+      Authorization: "Bearer #{token}"
+    }
+    response = Net::HTTP.get_response(uri, headers)
+    json = JSON.parse(response.body)
+    json.dig('tracks', 'items')
   end
 end
