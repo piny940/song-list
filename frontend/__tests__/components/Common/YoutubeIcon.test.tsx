f6fb14896a60d0669fc4bec03ee28b3edf943ffa
piny940 <an.s.shoma@gmail.com>
2023-06-26 21:30:40 +0900

    YoutubeIconのテストを作成

---
+++ frontend/__tests__/components/Common/YoutubeIcon.test.tsx
@@ -0,0 +1,14 @@
+import { render, waitFor } from '@testing-library/react'
+import { expect } from '@jest/globals'
+import { YoutubeIcon, YoutubeIconProps } from '@/components/Common/YoutubeIcon'
+import { Mock } from 'ts-mockery'
+
+describe('<YoutubeIcon />', () => {
+  it('正常に描画される', async () => {
+    const props = Mock.all<YoutubeIconProps>()
+    const component = render(<YoutubeIcon {...props} />)
+    await waitFor(() => {
+      expect(component).toBeTruthy()
+    })
+  })
+})
