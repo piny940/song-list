4df3836010392685aad60c2a837a25b22b839bea
piny940 <an.s.shoma@gmail.com>
2023-06-27 20:00:36 +0900

    test

--- backend/app/controllers/homes_controller.rb
+++ backend/app/controllers/homes_controller.rb
@@ -1,3 +1,9 @@
 class HomesController < ApplicationController
-  def show; end
+  def show
+    p 'hoge'
+    p ENV['GOOGLE_BUCKET']
+    p ENV['USER']
+    p ENV['GOOGLE_API_KEY']
+    p ENV.fetch('GOOGLE_API_KEY')
+  end
 end
