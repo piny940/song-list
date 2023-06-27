8808f70cd136d51d6b348f347570492e227f6365
piny940 <an.s.shoma@gmail.com>
2023-06-27 20:32:58 +0900

    不要なコードを削除

--- backend/app/controllers/homes_controller.rb
+++ backend/app/controllers/homes_controller.rb
@@ -1,9 +1,3 @@
 class HomesController < ApplicationController
-  def show
-    p 'hoge'
-    p ENV['GOOGLE_BUCKET']
-    p ENV['USER']
-    p ENV['GOOGLE_API_KEY']
-    p ENV.fetch('GOOGLE_API_KEY')
-  end
+  def show; end
 end
