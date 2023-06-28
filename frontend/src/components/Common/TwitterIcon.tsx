0064b6eb6013c108df650a65a6089be07e6ed40e
piny940 <an.s.shoma@gmail.com>
2023-06-28 23:14:22 +0900

    youtube, twitterのアイコンの色を修正

--- frontend/src/components/Common/TwitterIcon.tsx
+++ frontend/src/components/Common/TwitterIcon.tsx
@@ -1,17 +1,3 @@
-import { styled } from 'styled-components'
-
-const YoutubeIconSpan = styled.span`
-  width: 22px;
-  height: 22px;
-  background-image: url('/images/twitter.svg');
-  background-size: contain;
-  display: inline-block;
-
-  &:hover {
-    background-image: url('/images/twitter-blue.svg');
-  }
-`
-
 export const TwitterIcon: React.FC = () => {
-  return <YoutubeIconSpan role="button" />
+  return <span className="twitter-icon" role="button" />
 }
