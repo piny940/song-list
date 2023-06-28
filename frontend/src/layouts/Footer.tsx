d0503468cedaf3921b1934fc18665eef16387f97
piny940 <an.s.shoma@gmail.com>
2023-06-28 22:57:40 +0900

    Footerの色を修正

--- frontend/src/layouts/Footer.tsx
+++ frontend/src/layouts/Footer.tsx
@@ -1,3 +1,4 @@
+import { useTheme } from '@/context/ThemeProvider'
 import { useLogout } from '@/hooks/session'
 import { useUser } from '@/hooks/user'
 import Link from 'next/link'
@@ -5,9 +6,15 @@ import Link from 'next/link'
 export const Footer: React.FC = () => {
   const { data } = useUser()
   const { logout } = useLogout()
+  const { theme } = useTheme()
 
   return (
-    <footer className="footer text-center bg-secondary text-white p-4">
+    <footer
+      className={
+        'footer text-center text-white p-4 ' +
+        (theme === 'light' ? 'bg-secondary' : 'bg-body-secondary')
+      }
+    >
       {data?.user && (
         <>
           <Link href="/maintenance" className="text-white">
