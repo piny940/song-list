a3a3b5a31e4530636fb89fe42a9d0c12041c9db7
piny940 <an.s.shoma@gmail.com>
2023-07-28 20:44:05 +0900

    db:migrate

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_25_054457) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_28_113520) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,7 +64,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_25_054457) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: ""
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -73,6 +72,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_25_054457) do
     t.datetime "updated_at", null: false
     t.integer "kind", default: 0, null: false
     t.string "name", null: false
+    t.string "email", default: ""
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -87,6 +87,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_25_054457) do
     t.datetime "published_at", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "published", default: true, null: false
     t.index ["channel_id"], name: "index_videos_on_channel_id"
     t.index ["video_id"], name: "index_videos_on_video_id", unique: true
   end
