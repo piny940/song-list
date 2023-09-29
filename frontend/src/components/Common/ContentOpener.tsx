a2d12b638a127c5c399a05d3b018c74c361eabac
mikan <83708535+piny940@users.noreply.github.com>
2023-09-29 22:00:51 +0900

    ContentOpenerをbuttonタグにする

--- frontend/src/components/Common/ContentOpener.tsx
+++ frontend/src/components/Common/ContentOpener.tsx
@@ -2,7 +2,7 @@ import { MouseEventHandler } from 'react'
 import styles from '../../styles/common.module.scss'
 import { styled } from 'styled-components'
 
-const DetailButton = styled.a`
+const DetailButton = styled.button`
   --text-color: var(--bs-body-color);
   &:hover {
     --text-color: var(--bs-link-color);
@@ -29,12 +29,7 @@ export const ContentOpener: React.FC<ContentOpenerProps> = ({
   label,
 }) => {
   return (
-    <DetailButton
-      role="button"
-      className="small me-4"
-      onClick={onClick}
-      data-testid={testID}
-    >
+    <DetailButton className="small me-4" onClick={onClick} data-testid={testID}>
       <Triangle
         className={`${styles.animateFast} ${
           contentOpen ? styles.rotate90 : ''
