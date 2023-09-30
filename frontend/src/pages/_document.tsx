5f2e04912e8337496a8d574fc9bd08ce8a0f2b47
mikan <83708535+piny940@users.noreply.github.com>
2023-09-30 09:46:26 +0900

    htmlにlang属性を付与

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from 'next/document'
 
 function Document() {
   return (
-    <Html>
+    <Html lang="ja">
       <Head>
         <meta content="text/html; charset=UTF-8" httpEquiv="Content-Type" />
         <link
