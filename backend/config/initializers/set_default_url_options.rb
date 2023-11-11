f99d8ab6c24ca282a8ec695fa198f50c9d3648f2
piny940 <an.s.shoma@gmail.com>
2023-11-11 20:55:55 +0900

    protocolを修正

--- backend/config/initializers/set_default_url_options.rb
+++ backend/config/initializers/set_default_url_options.rb
@@ -1,5 +1,2 @@
 Rails.application.routes.default_url_options[:host] ||= ENV.fetch('RAILS_HOST', nil)
-if Rails.application.config.force_ssl || Rails.env.development?
-  Rails.application.routes.default_url_options[:protocol] =
-    'https'
-end
+Rails.application.routes.default_url_options[:protocol] = 'https'
