c104a60e24c6e8cb7f4aa1848261cc7ea8bf9901
piny940 <an.s.shoma@gmail.com>
2023-11-11 20:28:48 +0900

    sidekiqで実行する内容を設定

--- backend/app/jobs/create_recent_videos_song_items_job.rb
+++ backend/app/jobs/create_recent_videos_song_items_job.rb
@@ -2,6 +2,10 @@ class CreateRecentVideosSongItemsJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    p 'hoge'
+    p 'Start searching recent videos'
+    Channel.all.each(&:search_and_create_recent_videos)
+    p 'Start searching setlist'
+    Video.search_and_create_song_items!
+    p 'Completed searching setlist'
   end
 end
