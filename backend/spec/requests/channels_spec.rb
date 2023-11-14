1ce847484342c1a0c2e0e57ae84d808ecb72c4e7
piny940 <an.s.shoma@gmail.com>
2023-11-14 17:56:38 +0900

    channelのfactoryを使用

--- backend/spec/requests/channels_spec.rb
+++ backend/spec/requests/channels_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 describe Api::ChannelsController, type: :request do
-  fixtures :channels
-
   describe 'GET /api/channels' do
     it('正常に取得できる') do
+      10.times { create(:channel) }
+
       get '/api/channels'
 
       expect(response.status).to eq(200)
 
       json = response.parsed_body
       # publishedなチャンネルのみ取得する
-      expect(json['channels'].count).to eq 2
-      expect(json['channels'][0]['thumbnails']['medium']['width']).to eq 240
+      expect(json['channels'].count).to eq 10
+      expect(json['channels'][0]['thumbnails']['medium']['width']).to be_present
       expect(json['channels'][0]['created_at']).to be_present
       expect(json['channels'][0]['updated_at']).to be_present
       expect(json['channels'][0]['custom_name']).to be_present
@@ -20,13 +20,13 @@ describe Api::ChannelsController, type: :request do
   end
   describe 'GET /api/channels/:id' do
     it('正常に取得できる') do
-      one = channels(:shairu)
+      one = create(:channel)
       get "/api/channels/#{one.id}"
 
       expect(response.status).to eq(200)
 
       json = response.parsed_body
-      expect(json['channel']['thumbnails']['medium']['width']).to eq 240
+      expect(json['channel']['thumbnails']['medium']['width']).to be_present
       expect(json['channel']['custom_name']).to be_present
     end
   end
