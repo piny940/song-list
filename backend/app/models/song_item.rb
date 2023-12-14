cc16434f7e8c9e38b826ae06e729f251fca488ea
piny940 <an.s.shoma@gmail.com>
2023-12-14 19:10:11 +0900

    nullもunknowとみなす

--- backend/app/models/song_item.rb
+++ backend/app/models/song_item.rb
@@ -112,7 +112,7 @@ class SongItem < ApplicationRecord
 
     begin
       content = OpenAi.complete_chat(messages)
-      content = content.gsub(/(u|U)nknown|UNKNOWN|/, '')
+      content = content.gsub(/(u|U)nknown|UNKNOWN|null|NULL/, '')
       content = content.gsub(/"-"/, '""')
       JSON.parse(content)
     rescue StandardError
