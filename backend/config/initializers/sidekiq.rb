6872c7a87b0fba3ae28a8272e753f6fb3c281cc5
piny940 <83708535+piny940@users.noreply.github.com>
2024-06-02 10:59:56 +0900

    REDISの環境変数が設定されていない場合nilになるようにする

--- backend/config/initializers/sidekiq.rb
+++ backend/config/initializers/sidekiq.rb
@@ -1,13 +1,13 @@
 Sidekiq.configure_server do |config|
   config.redis = {
-    url: "redis://#{ENV.fetch('REDIS_HOST')}",
-    password: ENV.fetch('REDIS_PASSWORD')
+    url: "redis://#{ENV.fetch('REDIS_HOST', nil)}",
+    password: ENV.fetch('REDIS_PASSWORD', nil)
   }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
-    url: "redis://#{ENV.fetch('REDIS_HOST')}",
-    password: ENV.fetch('REDIS_PASSWORD')
+    url: "redis://#{ENV.fetch('REDIS_HOST', nil)}",
+    password: ENV.fetch('REDIS_PASSWORD', nil)
   }
 end
